a887bdd85e36e092f8a9d745d322743031cc878f
rwv1001 <rwv1001@gmail.com>
2026-02-20 20:24:57 +0000

    Fixed kea-lfc: running lease file cleanup error

--- kea/entrypoint.d/10-fixup.sh
+++ kea/entrypoint.d/10-fixup.sh
@@ -3,5 +3,12 @@ set -eu
 
 rm -f /usr/local/var/run/kea/dhcp4.kea-dhcp4.pid || true
 
+# kea-lfc is compiled to look for its logger lockfile in /run/kea, but
+# the Alpine kea package uses /usr/local/var/run/kea.  Symlink so both
+# the main process and kea-lfc share the same runtime directory.
+if [ ! -e /run/kea ]; then
+    ln -s /usr/local/var/run/kea /run/kea
+fi
+
 mkdir -p /kea/sockets
 chmod 750 /kea/sockets || true
